40c3f40dfa8b61d3a4f8dc2cfe968bda4b08a12d
spear97 <72108425+spear97@users.noreply.github.com>
2024-02-21 11:39:29 -0600

    Update Converter.py

--- RomanNumeral/Converter.py
+++ RomanNumeral/Converter.py
@@ -2,57 +2,68 @@ import re
 
 class Converter(object):
 
-    #Constructor
+    # Constructor
     def __init__(self):
+        '''
+        Constructor for the Converter class.
+        Initializes a dictionary of Roman Numeral values.
+        '''
+        self.Values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
-        #A Dictionary of Values that will be used to Convert Roman Numerals to a Numeric Value
-        self.Values = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-
-    #Performs Conversion from Roman Numerals to Decimal Values
-    def RomanNumeralToDecimal(self, romanNumeral:str = ''):
+    # Convert Roman Numerals to Decimal Values
+    def RomanNumeralToDecimal(self, romanNumeral: str = ''):
+        '''
+        Converts a Roman Numeral to its equivalent Decimal Value.
         
-        '''
-        Performs the RomanNumeral to Decimal Value Conversion for the Application
+        Args:
+            romanNumeral (str): The Roman Numeral to be converted.
+        
+        Returns:
+            int: The Decimal Value of the Roman Numeral.
         '''
 
-        #Keeps the Sum for the value of the Roman Numeral
+        # Initialize the sum for the value of the Roman Numeral
         sum = 0
 
-        #Check if the entered String is greater than 0
+        # Check if the entered string is greater than 0
         if len(romanNumeral) > 0:
 
-            #Interate through the Entered Roman Numeral
+            # Iterate through the entered Roman Numeral
             for i in range(len(romanNumeral) - 1):
 
-                #The Current Character in romanNumeral
+                # The current character in romanNumeral
                 left = romanNumeral[i]
 
-                #The Next Character in romanNumeral
+                # The next character in romanNumeral
                 right = romanNumeral[i + 1]
 
-                #If next Character in romanNumeral is greater than than the current, perform subtraction from sum by the left Value
+                # If next character in romanNumeral is greater than the current, subtract left value from sum
                 if self.Values[left] < self.Values[right]:
                     sum -= self.Values[left]
-
-                #For all other other cases, perform addition to sum by the left Value
                 else:
+                    # For all other cases, add left value to sum
                     sum += self.Values[left]
 
-            #Add the Value of the Last Character from Roman Numerals to the total sum to prevent from numeric value being a negative value
+            # Add the value of the last character from Roman Numerals to the total sum
             sum += self.Values[romanNumeral[-1]]
 
-        #Return the value of sum
+        # Return the sum
         return sum
 
-    #Determine if entered Roman Numeral is Valid
-    def valid_input(self, romanNumeral:str = ''):
+    # Check if entered Roman Numeral is valid
+    def valid_input(self, romanNumeral: str = ''):
+        '''
+        Checks if the input Roman Numeral is valid.
+        
+        Args:
+            romanNumeral (str): The Roman Numeral to be validated.
+        
+        Returns:
+            bool: True if the input is a valid Roman Numeral, False otherwise.
+        '''
 
-        # Define a regular expression to match valid Roman numerals
+        # Define a regular expression pattern to match valid Roman numerals
         pattern = '^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$'
 
         # Use the re module to search for a match
         return bool(re.match(pattern, romanNumeral))
-
-
-
-
